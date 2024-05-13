7eab2c04e3be9bebb8f3db50f5c05c54cb7a95d1
Lamya Faizal Salma <130156095+LamyaFS@users.noreply.github.com>
2024-05-13 12:09:24 +0400

    Lfire

---
+++ src/app/firebase.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FireserviceService {
+  constructor(
+    public firestore: AngularFirestore,
+    public auth: AngularFireAuth
+  ) {}
+  loginWithEmail(Details) {
+    return this.auth.signInWithEmailAndPassword(Details.email, Details.password);
+  }
+
+  signup(Details) {
+    return this.auth.createUserWithEmailAndPassword(Details.email, Details.password);
+  }
+
+  saveDetails(Details) {
+    return this.firestore.collection("users").doc(Details.uid).set(Details);
+  }
+  getDetails(Details) {
+    return this.firestore.collection("users").doc(Details.uid).valueChanges();
+  }
+}
