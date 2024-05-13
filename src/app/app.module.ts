7be1c3c9317fdfc9b2d1a62e0e2d070a651778ec
Lamya Faizal Salma <130156095+LamyaFS@users.noreply.github.com>
2024-05-13 11:21:46 +0400

    Lfirebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,22 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import{initializeApp} from 'firebase/app';
-const firebaseConfig = {
-  apiKey: "AIzaSyCv8oOMIP6OR6T-twIKqyAanT3qoTPRC3w",
-  authDomain: "swapcycle-f5400.firebaseapp.com",
-  projectId: "swapcycle-f5400",
-  storageBucket: "swapcycle-f5400.appspot.com",
-  messagingSenderId: "615718133563",
-  appId: "1:615718133563:web:5044123db87784ec8d211e"
-};
 
-const app = initializeApp(firebaseConfig);
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyCv8oOMIP6OR6T-twIKqyAanT3qoTPRC3w",
+    authDomain: "swapcycle-f5400.firebaseapp.com",
+    projectId: "swapcycle-f5400",
+    storageBucket: "swapcycle-f5400.appspot.com",
+    messagingSenderId: "615718133563",
+    appId: "1:615718133563:web:5044123db87784ec8d211e"
+  }
+  
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
